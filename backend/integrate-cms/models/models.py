f3ea9b8deaf22ec8b4ecbb47dfc6f01b6fc5bb23
pranavkrishna-sn <pranav05krish@gmail.com>
2025-11-27 11:59:06 +0530

    Created file via n8n

---
+++ backend/integrate-cms/models/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class IntegrateCms(Base):
+    __tablename__ = 'integrate_cms'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
