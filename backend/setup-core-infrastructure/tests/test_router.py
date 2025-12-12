bd5033d95106d4eec116accd8d18f8feded2847b
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-12 14:34:26 +0530

    Delete backend directory

--- backend/setup-core-infrastructure/tests/test_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi.testclient import TestClient
-from main import app
-
-client = TestClient(app)
-
-def test_setup_core_infrastructure_router():
-    response = client.get('/setup-core-infrastructure')
-    assert response.status_code == 200
