335fb934ccab5e02e4ac1211732c1541fda86619
pranavkrishna05 <pranav2002krishna@gmail.com>
2026-01-06 21:07:59 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from backend.controllers.auth.user_controller import user_bp
+from backend.controllers.products.product_controller import product_bp
 from backend.config.logging_config import setup_logging
 
 def create_app() -> Flask:
     app = Flask(__name__)
     setup_logging()
     app.register_blueprint(user_bp, url_prefix='/auth')
+    app.register_blueprint(product_bp, url_prefix='/products')
     return app
 
 if __name__ == '__main__':
