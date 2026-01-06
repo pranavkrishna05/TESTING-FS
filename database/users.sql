7176a5bdd108d1e53d05cecc710d4e33afdb6b51
pranavkrishna05 <pranav2002krishna@gmail.com>
2026-01-06 21:11:15 +0530

    commited by n8n

--- database/users.sql
+++ database/users.sql
@@ -4,10 +4,6 @@ CREATE TABLE users (
     password TEXT NOT NULL,
     first_name TEXT,
     last_name TEXT,
-    phone_number TEXT,
-    address TEXT,
-    login_attempts INTEGER NOT NULL DEFAULT 0,
-    is_locked BOOLEAN NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
