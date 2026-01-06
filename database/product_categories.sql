9dfcb04aafa8837af0a66bd3bd4e0338220af02c
pranavkrishna05 <pranav2002krishna@gmail.com>
2026-01-06 21:55:16 +0530

    commited by n8n

---
+++ database/product_categories.sql
@@ -0,0 +1,7 @@
+CREATE TABLE product_categories (
+    product_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    PRIMARY KEY (product_id, category_id),
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
